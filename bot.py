e647b2d84e8bca4f2ffa793d91eb8ad290fa78f4
aka-tinyy <73321003+aka-tinyy@users.noreply.github.com>
2022-04-17 16:33:31 -0400

    updated bot.py
    Made it an admin only command with option to specify which guilds the bot registers slash commands in.

--- bot.py
+++ bot.py
@@ -14,20 +14,24 @@ discord_token = ""
 
 #add your nitrado token in the string
 nitrado_token = ""
+#you can also replace line 20 with the line below if you only want the command to register in one server. Make sure to seperate guild ids with a comma
+#@bot.slash_command(guild_ids=[]) 
 
 @bot.slash_command()
 async def unban_all(ctx: commands.Context, id: Option(str, "Enter the server id to unball all players on")):
-    async with aiohttp.ClientSession() as session:
-        headers = {'Authorization': nitrado_token}
-        async with session.get(f"https://api.nitrado.net/services/{id}/gameservers/games/banlist", headers=headers) as response:   
-            data = (await response.json())['data']['banlist']
-            players = {player['name'] for player in data}
-            for player in players:
-                gamertag = {'identifier': player}
-                async with aiohttp.ClientSession() as s:
-                    async with s.delete(f"https://api.nitrado.net/services/{id}/gameservers/games/banlist", headers=headers, data=json.dumps(gamertag)) as t:  
-                        await ctx.respond(t)
-
+    if ctx.author.guild_permissions.administrator:
+        async with aiohttp.ClientSession() as session:
+            headers = {'Authorization': nitrado_token}
+            async with session.get(f"https://api.nitrado.net/services/{id}/gameservers/games/banlist", headers=headers) as response:   
+                data = (await response.json())['data']['banlist']
+                players = {player['name'] for player in data}
+                for player in players:
+                    gamertag = {'identifier': player}
+                    async with aiohttp.ClientSession() as s:
+                        async with s.delete(f"https://api.nitrado.net/services/{id}/gameservers/games/banlist", headers=headers, data=json.dumps(gamertag)) as t:  
+                            await ctx.respond(t)
+    else:
+        await ctx.respond("You need administrator to run this command")
 
 
 
